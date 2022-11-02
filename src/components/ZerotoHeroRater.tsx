f94f57c8281f286019817ecec68eaadbed68bd7a
jasveerhashtrust <jasveer@hashtrust.in>
2022-11-02 19:16:47 +0530

    improvements

--- src/components/ZerotoHeroRater.tsx
+++ src/components/ZerotoHeroRater.tsx
@@ -2,12 +2,12 @@
 import { useState } from "react";
 import "./stars.css";
 const Star = () => {
-    const [rating, setRating] = useState(0);
-    const [hover, setHover] = useState(0);
+    const [starRating, setstarRating] = useState(0);
+    const [drag, setdrag] = useState(0);
     return (
-        <div className="star-rating">
+        <div className="star-starRating">
             <div className="container">
-                <h1>Give us Rating</h1>
+                <h1>Give us starRating</h1>
                 <div>
                     {[...Array(5)].map((star, index) => {
                         index += 1;
@@ -15,18 +15,18 @@ const Star = () => {
                             <button
                                 type="button"
                                 key={index}
-                                className={index <= (hover || rating) ? "on" : "off"}
-                                onClick={() => setRating(index)}
-                                onMouseEnter={() => setHover(index)}
-                                onMouseLeave={() => setHover(rating)}
+                                className={index <= (drag || starRating) ? "on" : "off"}
+                                onClick={() => setstarRating(index)}
+                                onMouseEnter={() => setdrag(index)}
+                                onMouseLeave={() => setdrag(starRating)}
                             >
                                 <span className="star">&#9733;</span>
                             </button>
                         );
                     })}
                 </div>
-                <div className="rating">
-                    Rating : {rating}
+                <div className="starRating">
+                    starRating : {starRating}
                 </div>
             </div>
         </div>
